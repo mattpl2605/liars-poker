fdeecbb7096a968d7f8561d38168dd60b564aef1
Matthew Port Louis <mattpl2605@gmail.com>
2025-07-01 01:01:02 -0500

    socket: remove pure-websocket call, allow polling fallback

--- liars-poker/lib/socket.ts
+++ liars-poker/lib/socket.ts
@@ -9,11 +9,10 @@ let socket: Socket | null = null;
 
 export function getSocket() {
   if (!socket && URL) {
-   socket = io(URL, { transports: ['websocket'] });   // <- too strict
-   socket = io(URL, {
-     transports: ['websocket', 'polling'], // allow fallback, then upgrade
-     secure: true                          // tells client to use wss://
-   });
+    socket = io(URL, {
+      transports: ['websocket', 'polling'], // allow polling handshake, then upgrade
+      secure: true                          // wss:// when URL is https
+    });
   }
   return socket;
 }
